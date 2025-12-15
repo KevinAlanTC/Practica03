465176e916f794b1852d845612d6db488f118bc3
KATC <323160698@comunidad.unam.mx>
2025-12-15 01:58:27 -0600

    Create Arithmetic.java

---
+++ Java_Inheritance_II/Arithmetic.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+//Write your code here
+class Arithmetic
+{
+    int add(int x, int y)
+    {
+        return x+y;
+    }
+}
+
+class Adder extends Arithmetic
+{
+
+}
